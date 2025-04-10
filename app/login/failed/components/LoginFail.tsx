61d095ce3618697d72116a4e69b1fbe60aaf7c37
jxchenscut <jxchenscut@gmail.com>
2025-04-10 21:45:36 +0800

    Update LoginFail.tsx

--- app/login/failed/components/LoginFail.tsx
+++ app/login/failed/components/LoginFail.tsx
@@ -11,3 +11,12 @@ export function LoginFail({ errorMessage }: { errorMessage: string }) {
     </div>
   );
 }
+
+import dynamic from "next/dynamic";
+import type { FC } from "react";
+
+const LoginFail = dynamic<FC>(() => import("./components/LoginFail"), {
+  ssr: false,
+});
+
+export default LoginFail;
